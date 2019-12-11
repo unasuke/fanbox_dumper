1c0090dc0df810836a265294620cba9c132c28b6
Yusuke Nakamura <yusuke1994525@gmail.com>
2019-12-11 21:46:05 +0900

    Add script dumper.js

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## find_fanbox_article_url.rb
 このscriptは、<https://www.pixiv.net/fanbox/manage/post/published>の内容から、pixivFANBOXに投稿した記事のリストをJSONで抽出します。
 
+## dumper.js
+このscriptは、URLの配列から数秒の間隔をあけてページを開き、内容を保存して閉じます。Webブラウザのconsoleにペーストして実行します。
---
+++ dumper.js
@@ -0,0 +1,22 @@
+(function(window, document, location) {
+  'use strict';
+
+  // 取得したいURLの配列
+  const urls = [""]
+
+  urls.forEach(function(item, index, array) {
+    setTimeout(function(window,item){
+      let page = window.open(item);
+      setTimeout(function(page,window, item){
+        let snapshot = new XMLSerializer().serializeToString(page.document);
+        let a = document.createElement('a');
+        a.href = window.URL.createObjectURL(new Blob([snapshot]))
+        a.download = decodeURI(item).replace(/\//g,'__').replace(/#/g,'--') + '.html';
+        a.click();
+        page.close();
+      }, 10000, page, window,item); // ページを開いて10秒後に内容を保存
+      page.blur();
+      window.focus();
+    }, index * 3000, window,item); // 3秒おきにページを開く
+  })
+})(window, document, location);
